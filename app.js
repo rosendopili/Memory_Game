61802e04f80d939bab9903e577b37b89cdeed895
rosendopili <rosendopili@gmail.com>
2020-04-10 23:00:17 -0400

    first commit. main file structure and bulk of code

---
+++ app.js
@@ -0,0 +1,126 @@
+document.addEventListener('DOMContentLoaded', () =>{
+
+  const cardArray = [
+    {
+      name: 'astronaut',
+      img: 'images/astronaut.png '
+    },
+    {
+      name: 'astronaut',
+      img: 'images/astronaut.png '
+    },
+    {
+      name: 'atomic',
+      img: 'images/atomic.png '
+    },
+    {
+      name: 'atomic',
+      img: 'images/atomic.png '
+    },
+    {
+      name: 'chemistry',
+      img: 'images/chemistry.png '
+    },
+    {
+      name: 'chemistry',
+      img: 'images/chemistry.png '
+    },
+    {
+      name: 'microscope',
+      img: 'images/microscope.png '
+    },
+    {
+      name: 'microscope',
+      img: 'images/microscope.png '
+    },
+    {
+      name: 'poison',
+      img: 'images/poison.png '
+    },
+    {
+      name: 'poison',
+      img: 'images/poison.png '
+    },
+    {
+      name: 'rocket',
+      img: 'images/rocket.png '
+    },
+    {
+      name: 'rocket',
+      img: 'images/rocket.png '
+    }
+  ]
+
+  cardArray.sort(() => 0.5 - Math.random())
+
+  const grid = document.querySelector('.grid')
+  const resultDisplay = document.querySelector('#result')
+  const attemptDisplay = document.querySelector('#attempt')
+  let attemptCount = 0
+  let cardsChosen = []
+  let cardsChosenId = []
+  const cardsWon = []
+  const applause = new Audio("/sounds/applause.wav")
+  const blop = new Audio("/sounds/blop.wav")
+
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      let card = document.createElement('img')
+      card.setAttribute('src', 'images/pattern.jpg')
+      card.setAttribute('class', 'game-card')
+      card.setAttribute('data-id', i)
+      card.addEventListener('click', flipCard)
+      grid.appendChild(card)
+    }
+  }
+
+  function checkForMatch() {
+    let cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    if (cardsChosen[0] === cardsChosen[1]) {
+      alert('You found a match')
+      cards[optionOneId].setAttribute('src', 'images/white.jpg')
+      cards[optionTwoId].setAttribute('src', 'images/white.jpg')
+      cardsWon.push(cardsChosen)
+    } else {
+      cards[optionOneId].setAttribute('src', 'images/pattern.jpg')
+      cards[optionTwoId].setAttribute('src', 'images/pattern.jpg')
+      alert('Sorry, try again')
+    }
+
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if  (cardsWon.length === cardArray.length/2) {
+      resultDisplay.textContent = 'Congratulations! You found them all!'
+      applause.play()
+    }
+  }
+
+  //flip your card
+  function flipCard() {
+    let cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    blop.play()
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500)
+      attemptCounter()
+    }
+  }
+
+  function attemptCounter() {
+    attemptCount++
+    attemptDisplay.textContent = attemptCount
+  }
+
+  document.getElementById("reset").onclick = function reset() {
+    window.location.reload()
+  };
+
+createBoard()
+  //current code allows you to choose boxes that have already been matched. code allows you to double click an image and receive a match.  will need to lock the correct selections.
+
+})
